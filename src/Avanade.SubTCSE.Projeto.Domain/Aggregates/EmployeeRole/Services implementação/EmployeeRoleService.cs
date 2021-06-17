9461026acd39193899f8f730a172760c7bbe8712
moraes-caroline <moraes.caroline@hotmail.com>
2021-06-17 09:00:10 -0300

    aula 5

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Services implementação/EmployeeRoleService.cs
@@ -0,0 +1,12 @@
+﻿using Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Services_Contr;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Services_implementação
+{
+    public class EmployeeRoleService : IEmployeeRoleService //herança: implementar metodos da interface
+    {
+        public void AddEmployeeRole(Entities.EmployeeRole employeeRole) //implementação da interface
+        {
+            throw new System.NotImplementedException("calma o coração"); //exception de método não implementado
+        }
+    }
+}
